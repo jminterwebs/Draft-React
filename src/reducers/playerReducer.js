5990a49f9569b291a97d4930b5b659cf7dfd2ea1
Unknown <jmverdone@gmail.com>
2017-09-23 18:31:58 -0400

    filters out drafted players

--- src/reducers/playerReducer.js
+++ src/reducers/playerReducer.js
@@ -8,9 +8,12 @@ export default function playerReducer(state={loading: false, players:[]}, action
       let draftIndex = state.players.findIndex(function(player){
           return player.id === action.payload.id
         })
+        state.players[draftIndex].team_id = action.payload.team_id
+      state.players = state.players.filter((player)=>{
+          return player.team_id === null
+        })
 
-      state.players[draftIndex].team_id = action.payload.team_id
-      console.log(state.players[draftIndex])
+      return state
     default:
       return state;
   }
