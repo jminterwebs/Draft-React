5451dff25cfe3a36458e785ae7a59fafd8725ddb
Unknown <jmverdone@gmail.com>
2017-09-24 11:10:01 -0400

    assinging static team_id

--- src/reducers/playerReducer.js
+++ src/reducers/playerReducer.js
@@ -1,3 +1,5 @@
+import update from 'immutability-helper'
+
 export default function playerReducer(state={loading: false, players:[]}, action) {
   switch ( action.type ) {
     case 'LOADING_PLAYERS':
@@ -8,12 +10,21 @@ export default function playerReducer(state={loading: false, players:[]}, action
       let draftIndex = state.players.findIndex(function(player){
           return player.id === action.payload.id
         })
-        state.players[draftIndex].team_id = action.payload.team_id
-      state.players = state.players.filter((player)=>{
-          return player.team_id === null
-        })
+      //  state.players[draftIndex].team_id = action.payload.team_id
+      // state.players = state.players.filter((player)=>{
+      //     return player.team_id === null
+      //   })
+      // update(state.players[draftIndex], {team_id: {$apply: function(x){return x = action.payload.team_id}}})
 
-      return state
+    state = update(state, {
+        players: {
+          [action.payload.id]: {
+            team_id: {$set: action.payload.team_id}
+          }
+        }
+      }
+    )
+    return state
     default:
       return state;
   }
