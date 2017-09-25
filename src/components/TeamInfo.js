d431f98f8971531a14ce94753b4c706c957268c7
Unknown <jmverdone@gmail.com>
2017-09-24 20:08:42 -0400

    Drafted players will be listed

--- src/components/TeamInfo.js
+++ src/components/TeamInfo.js
@@ -1,13 +1,41 @@
-import React from 'react'
+import React, {Component} from 'react'
+import { connect } from 'react-redux'
 
 
 
-const TeamInfo = () => {
 
+class TeamInfo extends Component {
+  constructor(props){
+    super(props)
+
+    this.handleOnClick = this.handleOnClick.bind(this)
+  }
+
+ componentWillMount(){
+    let team = this.props.teamMembers.filter((player)=> {
+      return player.team_id == this.props.team_id
+    })
+    return team
+  }
+  handleOnClick(){
+    console.log(this.props)
+  }
+
+  render(){
   return (
-    <h3>TeamInfo Area</h3>
-
-  )
+    <div>
+      {this.props.teamMembers.filter((player)=>{
+        if(this.props.team_id != null){
+          return player.team_id == this.props.team_id
+        }
+      }).map((player)=> {
+        return( <h3>{player.first_name}</h3>)
+      })}
+    </div>
+  )}
+}
+function mapStateToProps(state){
+  return {teamMembers: state.players.players}
 }
 
-export default TeamInfo
+export default connect(mapStateToProps)(TeamInfo)
