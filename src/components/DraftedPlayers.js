740baa19f31f4a43a60b696dd97c73c0ebe2400a
Unknown <jmverdone@gmail.com>
2017-09-24 20:08:08 -0400

    Drafted teams area built out

--- src/components/DraftedPlayers.js
+++ src/components/DraftedPlayers.js
@@ -1,16 +1,25 @@
 import React, {Component} from 'react'
+import TeamInfo from './TeamInfo'
 
 
-class DraftPlayers extends Component {
-  constructor(props){
-    super(props)
+class DraftedPlayers extends Component {
 
 
-  }
 
 render(){
+
   return(
-    <div className='team'>
+    <div className='teamsWrapper'>
+        <TeamInfo className='teamMembers' team_id={1}/>
+        <TeamInfo className='teamMembers' team_id={2}/>
+        <TeamInfo className='teamMembers' team_id={3}/>
+        <TeamInfo className='teamMembers' team_id={4}/>
+        <TeamInfo className='teamMembers' team_id={5}/>
+        <TeamInfo className='teamMembers' team_id={6}/>
+        <TeamInfo className='teamMembers' team_id={7}/>
+        <TeamInfo className='teamMembers' team_id={8}/>
+        <TeamInfo className='teamMembers' team_id={9}/>
+        <TeamInfo className='teamMembers' team_id={10}/>
 
     </div>
 
@@ -20,4 +29,7 @@ render(){
 
 }
 
-export default DraftPlayers
+
+
+
+export default DraftedPlayers
