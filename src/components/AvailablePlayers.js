cf8ff10ddb73f6b20223e6340e0f716b73fd4bda
Unknown <jmverdone@gmail.com>
2017-09-26 19:57:13 -0400

    moved fetch to <App/>

--- src/components/AvailablePlayers.js
+++ src/components/AvailablePlayers.js
@@ -3,19 +3,13 @@ import DraftPlayer from './DraftPlayer.js'
 import PlayerInfo from './PlayerInfo'
 import TeamInfo from './TeamInfo'
 import DraftedPlayers from './DraftedPlayers'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import * as actions from '../actions/fetchPlayers.js'
+
 
 
 
   class AvailablePlayers extends Component {
 
-    componentWillMount(){
-       if(!this.props.players){
-        this.props.actions.fetchPlayers()
-     }
-    }
+
 
 
 
@@ -47,14 +41,8 @@ import * as actions from '../actions/fetchPlayers.js'
   }
 }
 
-  function mapStateToProps(state) {
-    return { availablePlayers: state.players.players}
-  }
-
-  function mapDispatchToProps(dispatch){
-      return {actions: bindActionCreators(actions, dispatch)}
-  }
 
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(AvailablePlayers)
+
+export default AvailablePlayers
