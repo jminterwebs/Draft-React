5d681df45815d864f9e8970dc801c73b5f8d46d7
Unknown <jmverdone@gmail.com>
2017-09-26 22:43:28 -0400

    added redux connection

--- src/components/TeamInfo.js
+++ src/components/TeamInfo.js
@@ -1,29 +1,18 @@
 import React, {Component} from 'react'
-import { connect } from 'react-redux'
 
 
 
 
 
-class TeamInfo extends Component {
-  
 
- componentWillMount(){
-    let team = this.props.teamMembers.filter((player)=> {
-      return player.team_id == this.props.team_id
-    })
-    return team
-  }
+const TeamInfo =(props) => {
 
-  render(){
   return (
-
-
       <div className='teamMembers'>
         <input className="teamBox" placeholder="Team Name" type="text"></input>
-        {this.props.teamMembers.filter((player)=>{
-          if(this.props.team_id != null){
-            return player.team_id == this.props.team_id
+        {props.teamMembers.filter((player)=>{
+          if(props.team_id != null){
+            return player.team_id == props.team_id
           }
         }).map((player)=> {
           return( <li className={'playerMember ' + player.position} ><h2>{player.first_name}</h2><h2> {player.last_name}</h2></li>)
@@ -31,9 +20,7 @@ class TeamInfo extends Component {
       </div>
 
   )}
-}
-function mapStateToProps(state){
-  return {teamMembers: state.players.players}
-}
 
-export default connect(mapStateToProps)(TeamInfo)
+
+
+export default TeamInfo
