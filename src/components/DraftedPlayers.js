45deae8b952924806da829f05a2fe3ee1a05f8de
Unknown <jmverdone@gmail.com>
2017-09-26 22:43:11 -0400

    removed redux connection

--- src/components/DraftedPlayers.js
+++ src/components/DraftedPlayers.js
@@ -1,35 +1,39 @@
 import React, {Component} from 'react'
 import TeamInfo from './TeamInfo'
-
-
-const DraftedPlayers = () => {
-
+import { connect } from 'react-redux'
 
 
 
-
-  return(
-    <div className='teamsWrapper'>
-        <TeamInfo team_id={1}/>
-        <TeamInfo team_id={2}/>
-        <TeamInfo team_id={3}/>
-        <TeamInfo team_id={4}/>
-        <TeamInfo team_id={5}/>
-        <TeamInfo team_id={6}/>
-        <TeamInfo team_id={7}/>
-        <TeamInfo team_id={8}/>
-        <TeamInfo team_id={9}/>
-        <TeamInfo team_id={10}/>
-
-    </div>
+class DraftedPlayers extends Component{
 
 
-  )
+
+
+render(){
+    return(
+      <div className='teamsWrapper'>
+          <TeamInfo team_id={1} teamMembers = {this.props.teamMembers}/>
+          <TeamInfo team_id={2} teamMembers = {this.props.teamMembers}/>
+          <TeamInfo team_id={3} teamMembers = {this.props.teamMembers}/>
+          <TeamInfo team_id={4} teamMembers = {this.props.teamMembers}/>
+          <TeamInfo team_id={5} teamMembers = {this.props.teamMembers}/>
+          <TeamInfo team_id={6} teamMembers = {this.props.teamMembers}/>
+          <TeamInfo team_id={7} teamMembers = {this.props.teamMembers}/>
+          <TeamInfo team_id={8} teamMembers = {this.props.teamMembers}/>
+          <TeamInfo team_id={9} teamMembers = {this.props.teamMembers}/>
+          <TeamInfo team_id={10} teamMembers = {this.props.teamMembers}/>
+
+      </div>
+
+
+    )
+    }
 }
 
 
 
+function mapStateToProps(state){
+  return {teamMembers: state.players.players}
+}
 
-
-
-export default DraftedPlayers
+export default connect(mapStateToProps)(DraftedPlayers)
