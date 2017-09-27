02d5a76ce0dda3e19fbabdfd24ec737c55bfa2b3
Unknown <jmverdone@gmail.com>
2017-09-26 21:14:09 -0400

    moved player details to stateless component

--- src/components/PlayerInfo.js
+++ src/components/PlayerInfo.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PlayerDetails from './PlayerDetails'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import * as actions from '../actions/draftPlayer.js'
@@ -15,16 +16,12 @@ class  PlayerInfo extends Component {
     }
 
 
+
   render(){
     return(
-    <div className="playerInfoContainer">
-      <div className="playerInfo">
-        <h3 className="playerInfoName">Name: {this.props.selectedPlayer.first_name} {this.props.selectedPlayer.last_name}</h3>
-        <h2 className="playerInfoPosition">Position: {this.props.selectedPlayer.position}</h2>
-        <h2 className="playerInfoRank">  Rank: {this.props.selectedPlayer.rank}</h2>
-        <h2 className="playerInfoProjectedPoints">Projected Points: {this.props.selectedPlayer.projected_points}</h2>
-      </div>
 
+      <div className="playerInfoContainer">
+      <PlayerDetails selectedPlayer={this.props.selectedPlayer}/>
       <div className="playerInfoDraftButton" onClick={()=>this.handleOnClick()}><h3>DRAFT PLAYER </h3></div>
     </div>
   )
