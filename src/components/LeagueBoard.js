3527773e9da71e3483bc8b00851c76bc887d806b
Unknown <jmverdone@gmail.com>
2017-09-30 16:55:50 -0400

    team board stubbed out

---
+++ src/components/LeagueBoard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const LeagueBoard = (props) => {
+
+
+return (
+  <div> This is a league Draft Board Stub
+    {props.match.params.id}
+  </div>
+
+)
+
+}
+
+
+export default LeagueBoard
