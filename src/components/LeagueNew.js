55955d378852278196b7942c9566e4ff7582f9f6
Unknown <jmverdone@gmail.com>
2017-10-01 10:21:43 -0400

    new league form stubbed out

--- src/components/LeagueNew.js
+++ src/components/LeagueNew.js
@@ -4,7 +4,24 @@ const LeagueNew = (props) => {
 
 
 return (
-  <div>This is the create new league component</div>
+  <div>
+    <form>
+      <input type="text" placeholder="League Name"/>
+      <input type="text" placeholder="Team One"/>
+      <input type="text" placeholder="Team Two"/>
+      <input type="text" placeholder="Team Three"/>
+      <input type="text" placeholder="Team Four"/>
+      <input type="text" placeholder="Team Five"/>
+      <input type="text" placeholder="Team Six"/>
+      <input type="text" placeholder="Team Seven"/>
+      <input type="text" placeholder="Team Eight"/>
+      <input type="text" placeholder="Team Nine"/>
+      <input type="text" placeholder="Team Ten"/>
+
+      <input type="submit"/>
+    </form>
+
+  </div>
 
 
 )
