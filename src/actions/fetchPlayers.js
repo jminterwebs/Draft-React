cb40a30397ea1e144e484f0f1f15dd5bc382802d
Unknown <jmverdone@gmail.com>
2017-10-01 13:48:11 -0400

    fetch players pulls from league players now

--- src/actions/fetchPlayers.js
+++ src/actions/fetchPlayers.js
@@ -1,13 +1,13 @@
 import fetch from 'isomorphic-fetch';
-export function fetchPlayers() {
+export function fetchPlayers(id) {
 
   return function(dispatch){
     dispatch({type: 'LOADING_PLAYERS'})
-    return fetch('http://localhost:3001/players')
+    return fetch(`http://localhost:3001/leagues/${id}`)
       .then(res => {return res.json()})
       .then(responseJson => {
         dispatch({type: 'FETCH_PLAYERS', payload: responseJson})
     })
-    
+
   }
 }
