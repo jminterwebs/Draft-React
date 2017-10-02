513c713e88ad70c963e70bfa31431e61719c981a
Unknown <jmverdone@gmail.com>
2017-10-02 19:05:48 -0400

    comment out

--- src/reducers/leagueReducer.js
+++ src/reducers/leagueReducer.js
@@ -1,4 +1,4 @@
-import update from 'immutability-helper'
+// import update from 'immutability-helper'
 
 export default function leagueReducer(state= {loading: false, leagues:[]}, action) {
   switch (action.type){
