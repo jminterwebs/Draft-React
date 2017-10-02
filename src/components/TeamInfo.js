304cf2f05dfccfffbb5e83500f9717f0e5f13595
Unknown <jmverdone@gmail.com>
2017-10-01 20:28:26 -0400

    team info now displays team name from teams

--- src/components/TeamInfo.js
+++ src/components/TeamInfo.js
@@ -9,14 +9,7 @@ const TeamInfo =(props) => {
 
   return (
       <div className='teamMembers'>
-        <input className="teamBox" placeholder="Team Name" type="text"></input>
-        {props.teamMembers.filter((player)=>{
-          if(props.team_id != null){
-            return player.team_id == props.team_id
-          }
-        }).map((player)=> {
-          return( <li className={'playerMember ' + player.position} ><h2>{player.first_name}</h2><h2> {player.last_name}</h2></li>)
-        })}
+        {props.teamInfo.name}
       </div>
 
   )}
