b601870921165b4df38453df0168fff1bf1f3cfc
Unknown <jmverdone@gmail.com>
2017-10-01 20:27:35 -0400

    refactored to pass teams as props

--- src/components/DraftedPlayers.js
+++ src/components/DraftedPlayers.js
@@ -1,39 +1,32 @@
 import React, {Component} from 'react'
 import TeamInfo from './TeamInfo'
-import { connect } from 'react-redux'
-
-
-
-class DraftedPlayers extends Component{
 
 
 
 
-render(){
+const DraftedPlayers = (props) =>{
+
     return(
+
+
       <div className='teamsWrapper'>
 
-          
-          <TeamInfo team_id={1} teamMembers = {this.props.teamMembers}/>
-          <TeamInfo team_id={2} teamMembers = {this.props.teamMembers}/>
-          <TeamInfo team_id={3} teamMembers = {this.props.teamMembers}/>
-          <TeamInfo team_id={4} teamMembers = {this.props.teamMembers}/>
-          <TeamInfo team_id={5} teamMembers = {this.props.teamMembers}/>
-          <TeamInfo team_id={6} teamMembers = {this.props.teamMembers}/>
-          <TeamInfo team_id={7} teamMembers = {this.props.teamMembers}/>
-          <TeamInfo team_id={8} teamMembers = {this.props.teamMembers}/>
-          <TeamInfo team_id={9} teamMembers = {this.props.teamMembers}/>
-          <TeamInfo team_id={10} teamMembers = {this.props.teamMembers}/>
+        {props.teams.map((team)=> {
+
+          return (
+            <TeamInfo teamInfo={team}/>
+          )
+
+        })
+      }
+
+
 
       </div>
 
 
     )
-    }
 }
 
-function mapStateToProps(state){
-  return {teamMembers: state.players.players}
-}
 
-export default connect(mapStateToProps)(DraftedPlayers)
+export default DraftedPlayers
