e8f220946e4b8e3d653f9f99380c7265c4564e7b
Unknown <jmverdone@gmail.com>
2017-10-05 11:26:30 -0400

    team infor reducer

---
+++ src/reducers/teamsInfoReducer.js
@@ -0,0 +1,27 @@
+import update from 'immutability-helper'
+
+export default function teamsInfoReducer( state= {loading: false, teamInfo: []}, action){
+  switch(action.type){
+    case 'LOADING_TEAMS':
+    return Object.assign({}, state, {loading:true})
+    case 'FETCH_TEAMS':
+    return {loading: false, teamInfo: action.payload}
+    case 'DRAFT_PLAYER':
+    let id = action.payload.drafting_team-1
+    console.log(action.payload)
+    console.log(state)
+    // state = update(state, {
+    //   teamInfo: {
+    //     id:{
+    //       players: { $push: [action.payload.selectedPlayer]}
+    //     }
+    //   }
+    // })
+
+
+
+    default:
+    return state;
+
+  }
+}
