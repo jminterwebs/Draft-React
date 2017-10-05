267e9d09f84fddc07ff4c62121cac7d20ef58726
Unknown <jmverdone@gmail.com>
2017-10-04 21:14:13 -0400

    action now uses league info

--- src/actions/draftPlayer.js
+++ src/actions/draftPlayer.js
@@ -1,14 +1,22 @@
 import fetch from 'isomorphic-fetch';
-let team_id = 1;
-let round = 1;
-export const draftPlayer = (player) => {
+
+
+export const draftPlayer = (player, leagueInfo) => {
+  let  drafting_team = leagueInfo.draft_spot
+    if (drafting_team % 10 == 0) {
+        drafting_team = 10
+        }
+
+
 
   return function(dispatch){
 
   if (player)  {
-    return fetch(`http://localhost:3001/players/${player.id}`,{
+    return fetch(`http://localhost:3001/leagues/${leagueInfo.id}`,{
           method: "PATCH",
-          body: JSON.stringify({ team_id: team_id}),
+          body: JSON.stringify({ player: player.id,
+                                 drafting_team: drafting_team
+                                }),
           headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
@@ -16,23 +24,7 @@ export const draftPlayer = (player) => {
       }
 
     )
-        .then(responseJson => {dispatch({type: 'DRAFT_PLAYER', payload: {player, team_id}})
-      }).then(function(){
-
-        if (round == 1){
-          team_id++
-          if(team_id > 10){
-            team_id = 10
-            round = 2
-          }
-        } else if( round == 2 ){
-          team_id --
-          if(team_id < 1){
-            team_id = 1
-            round = 1
-          }
-        }
+        .then(responseJson => {dispatch({type: 'DRAFT_PLAYER', payload: {player, leagueInfo}})
       })
-    }
   }
-}
+}}
