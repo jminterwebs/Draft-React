ff6e24449e872cef69d142b7839bd92f856c8610
Unknown <jmverdone@gmail.com>
2017-10-09 22:54:05 -0400

    watch list componet

---
+++ src/components/TeamWatchList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TeamWatchList = (props) => {
+
+
+  return (
+    <div>
+        {props.teamInfo.currentTeam.currentTeam}
+
+    </div>
+  )
+}
+
+export default TeamWatchList
