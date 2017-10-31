d3d71c7e24730964bd2f1e9a906e9865ec2fc184
Unknown <jmverdone@gmail.com>
2017-10-30 23:56:55 -0400

    finding bugs

--- src/components/DraftedPlayers.js
+++ src/components/DraftedPlayers.js
@@ -12,10 +12,7 @@ const DraftedPlayers = props => {
 
     return(
       <div className='teamsWrapper'>
-        3213213
-        21
         {props.teams.teamInfo.map((team)=>{
-
           return (
             <TeamInfo key={team.id} teamInfo={team}/>
           )
