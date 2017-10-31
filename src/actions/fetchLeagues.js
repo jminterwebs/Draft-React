1f826542f38e9f7310aa2ae6b0146869353c9c7b
Unknown <jmverdone@gmail.com>
2017-10-30 23:32:45 -0400

    changed to live API

--- src/actions/fetchLeagues.js
+++ src/actions/fetchLeagues.js
@@ -3,7 +3,7 @@ export function fetchLeagues() {
 
   return function(dispatch){
     dispatch({type: 'LOADING_LEAGUES'})
-    return fetch('http://localhost:3001/leagues')
+    return fetch('https://obscure-taiga-54498.herokuapp.com/leagues')
       .then(res => {return res.json()})
       .then(responseJson => {
         dispatch({type: 'FETCH_LEAGUES', payload: responseJson})
--- src/actions/fetchTeams.js
+++ src/actions/fetchTeams.js
@@ -4,7 +4,7 @@ export const fetchTeams = (league_id, team) => {
 
 return function(dispatch){
   dispatch({type: 'LOADING_TEAMS'})
-    return  fetch(`http://localhost:3001/leagues/${league_id}/teams`)
+    return  fetch(`https://obscure-taiga-54498.herokuapp.com/${league_id}/teams`)
     .then(res => {return res.json()})
     .then(responseJson => {
       dispatch({type: 'FETCH_TEAMS', payload: responseJson})
