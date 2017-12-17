a2271214c26bf486a0c9d8c002afd78c042c7c64
Unknown <jmverdone@gmail.com>
2017-12-16 22:03:30 -0500

    changed to full_name

--- src/components/PlayerDetails.js
+++ src/components/PlayerDetails.js
@@ -7,7 +7,7 @@ const PlayerDetails = (props) => {
 return (
 
     <div className="playerInfo">
-      <h3 className="playerInfoName">Name: {props.selectedPlayer.first_name} {props.selectedPlayer.last_name}</h3>
+      <h3 className="playerInfoName">Name: {props.selectedPlayer.full_name} </h3>
       <h2 className="playerInfoPosition">Position: {props.selectedPlayer.position}</h2>
       <h2 className="playerInfoRank">  Rank: {props.selectedPlayer.rank}</h2>
       <h2 className="playerInfoProjectedPoints">Projected Points: {props.selectedPlayer.projected_points}</h2>
