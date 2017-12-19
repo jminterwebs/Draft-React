a2bb6e7f9454647bd47c1edbc06855ed5f593c95
Unknown <jmverdone@gmail.com>
2017-12-18 22:18:58 -0500

    fetch player written

--- src/actions/fetchPlayer.js
+++ src/actions/fetchPlayer.js
@@ -1,6 +1,13 @@
 import fetch from 'isomorphic-fetch'
 
 export function fetchPlayer(id) {
+  return function(dispatch){
+  dispatch({type: 'LOADING_PLAYER'})
+  return fetch(`http://localhost:3001/players/${id}`)
+    .then(res => {return res.json()})
+    .then(responseJson => {
+      dispatch({type: 'FETCH_PLAYER', playload: responseJson})
+    })
 
-
+  }
 }
