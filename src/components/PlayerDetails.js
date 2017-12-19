231c29aeb4c43caac40720cd011e36c952edb57b
Unknown <jmverdone@gmail.com>
2017-12-18 23:01:31 -0500

    refactored for new api

--- src/components/PlayerDetails.js
+++ src/components/PlayerDetails.js
@@ -7,10 +7,13 @@ const PlayerDetails = (props) => {
 return (
 
     <div className="playerInfo">
-      <h3 className="playerInfoName">Name: {props.selectedPlayer.full_name} </h3>
-      <h2 className="playerInfoPosition">Position: {props.selectedPlayer.position}</h2>
-      <h2 className="playerInfoRank">  Rank: {props.selectedPlayer.rank}</h2>
-      <h2 className="playerInfoProjectedPoints">Projected Points: {props.selectedPlayer.projected_points}</h2>
+      {console.log(props)}
+      {console.log("this")}
+      {console.log(props.selectedPlayer.player)}
+      <h3 className="playerInfoName">Name: {props.selectedPlayer.player.full_name} </h3>
+      <h2 className="playerInfoPosition">Position: {props.selectedPlayer.player.position}</h2>
+      <h2 className="playerInfoRank">  Rank: {props.selectedPlayer.player.rank}</h2>
+      <h2 className="playerInfoProjectedPoints">Projected Points: {props.selectedPlayer.player.projected_points}</h2>
     </div>
 
 )
