18188d49656463621a8d0fe1bd67d71a113e3d4e
Unknown <jmverdone@gmail.com>
2017-12-18 23:04:06 -0500

    refactored for new api endpoints

--- src/reducers/playerInfoReducer.js
+++ src/reducers/playerInfoReducer.js
@@ -1,8 +1,10 @@
 
-export default function  playerInfoReducer(state = {}, action) {
+export default function  playerInfoReducer(state = {loading: false, player: {}}, action) {
   switch(action.type){
+    case 'LOADING_PLAYER':
+      return Object.assign({}, state, {loading:true})
     case 'SELECT_PLAYER':
-      return action.player
+      return {loading: false, player: action.playload.player}
     case 'DRAFT_PLAYER':
       return ""
     default:
