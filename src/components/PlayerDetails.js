92ccac8c4dbf5d33ac6e8f21b833f97eb332f5fd
Unknown <jmverdone@gmail.com>
2017-12-20 20:51:32 -0500

    removed console logs

--- src/components/PlayerDetails.js
+++ src/components/PlayerDetails.js
@@ -7,9 +7,6 @@ const PlayerDetails = (props) => {
 return (
 
     <div className="playerInfo">
-      {console.log(props)}
-      {console.log("this")}
-      {console.log(props.selectedPlayer.player)}
       <h3 className="playerInfoName">Name: {props.selectedPlayer.player.full_name} </h3>
       <h2 className="playerInfoPosition">Position: {props.selectedPlayer.player.position}</h2>
       <h2 className="playerInfoRank">  Rank: {props.selectedPlayer.player.rank}</h2>
