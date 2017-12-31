3ad6f3425ec90e8423f17f10a016d794caea7dd6
Unknown <jmverdone@gmail.com>
2017-12-31 12:45:05 -0500

    custome response

--- src/actions/addLeague.js
+++ src/actions/addLeague.js
@@ -6,28 +6,25 @@ export const createLeague = (league) => {
 
     if(league){
 
-      fetch('https://obscure-taiga-54498.herokuapp.com/leagues/', {
+      fetch('http://localhost:3001/leagues/', {
         method: "POST",
+        mode: 'cors',
         body: JSON.stringify({
               league: {
                 name: league.name,
                 teams_attributes: league.teams_attributes
               }
         }),
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        }
+        headers: newLeageuHeaders
       })
+      .then(res => {console.log(res.json())})
+      .then(function(){console.log(newLeageuHeaders.get('league_id'))})
       .then( res => {dispatch({type: 'CREATE_LEAGUE', payload: {league}})})
-      .then(league => console.log(JSON.stringify(league)))
-
-
     }
-
-
   }
 
-
-
 }
+
+
+var newLeageuHeaders = new Headers({  'Accept': 'application/json',
+  'Content-Type': 'application/json'})
