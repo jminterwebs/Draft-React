a5f588f9934eab2328897542c1a3108e255d404c
Unknown <jmverdone@gmail.com>
2017-12-31 14:07:52 -0500

    custom response from backend

--- src/actions/addLeague.js
+++ src/actions/addLeague.js
@@ -9,6 +9,7 @@ export const createLeague = (league) => {
       fetch('http://localhost:3001/leagues/', {
         method: "POST",
         mode: 'cors',
+        redirect: 'follow',
         body: JSON.stringify({
               league: {
                 name: league.name,
@@ -17,9 +18,14 @@ export const createLeague = (league) => {
         }),
         headers: newLeageuHeaders
       })
-      .then(res => {console.log(res.json())})
-      .then(function(){console.log(newLeageuHeaders.get('league_id'))})
-      .then( res => {dispatch({type: 'CREATE_LEAGUE', payload: {league}})})
+      .then(res => Promise.all([res, res.json()]))
+      .then(([res, json]) => {
+        dispatch({type:'CREATE_LEAGUE', payload: {json}})
+        // if (res.redirected){
+        //   location.href = res.url
+        // }
+      })
+       // .then( res => {dispatch({type: 'CREATE_LEAGUE', payload: {league}})})
     }
   }
 
