14e6d8af722a784db791ec61e86cf7a376d1fda1
Unknown <jmverdone@gmail.com>
2018-01-02 20:10:05 -0500

    cors

--- src/actions/fetchLeagues.js
+++ src/actions/fetchLeagues.js
@@ -3,7 +3,12 @@ export function fetchLeagues() {
 
   return function(dispatch){
     dispatch({type: 'LOADING_LEAGUES'})
-    return fetch('https://obscure-taiga-54498.herokuapp.com/leagues')
+    return fetch('https://obscure-taiga-54498.herokuapp.com/leagues',{
+      mode: 'cors',
+      headers: {
+        'Access-Control-Allow-Origin' : '*'
+      }
+    })
       .then(res => {return res.json()})
       .then(responseJson => {
         dispatch({type: 'FETCH_LEAGUES', payload: responseJson})
