ae0169734ad4c33dc36e2de7ee4ba95ae2b97e7b
Unknown <jmverdone@gmail.com>
2018-01-02 20:03:42 -0500

    CORS

--- src/actions/fetchPlayer.js
+++ src/actions/fetchPlayer.js
@@ -3,7 +3,7 @@ import fetch from 'isomorphic-fetch'
 export function fetchPlayer(id) {
   return function(dispatch){
   dispatch({type: 'LOADING_PLAYER'})
-  return fetch(`http://localhost:3001/players/${id}`)
+  return fetch(`https://obscure-taiga-54498.herokuapp.com/leagues/${id}`)
     .then(res => {return res.json()})
     .then(responseJson => {
       dispatch({type: 'FETCH_PLAYER', playload: responseJson})
