50decd8367c189fb8556e2c4a0d02daa3a8101a9
Unknown <jmverdone@gmail.com>
2018-01-02 20:35:45 -0500

    bug fix

--- src/actions/fetchPlayer.js
+++ src/actions/fetchPlayer.js
@@ -3,7 +3,9 @@ import fetch from 'isomorphic-fetch'
 export function fetchPlayer(id) {
   return function(dispatch){
   dispatch({type: 'LOADING_PLAYER'})
-  return fetch(`https://obscure-taiga-54498.herokuapp.com/leagues/${id}`)
+  return fetch(`https://obscure-taiga-54498.herokuapp.com/leagues/players/${id}`,{
+    mode: 'cors'
+  })
     .then(res => {return res.json()})
     .then(responseJson => {
       dispatch({type: 'FETCH_PLAYER', playload: responseJson})
