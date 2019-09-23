45b00575f382c296bd80c73179c42ddeb884cda6
John Verdone <jmverdone@gmail.com>
2019-09-22 21:00:15 -0400

    draft reducer

--- src/reducers/playersReducer.js
+++ src/reducers/playersReducer.js
@@ -1,5 +1,14 @@
+/* eslint-disable no-console */
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-case-declarations */
+import update from 'immutability-helper';
+
 export default function playerListReducer(
-  state = { loading: false, players: [] },
+  state = {
+    loading: false,
+    players: [],
+    draftedInfo: { round: 1, selection: 0, team: 1 }
+  },
 
   action
 ) {
@@ -7,7 +16,48 @@ export default function playerListReducer(
     case 'LOADING_PLAYERS':
       return { ...state, loading: true };
     case 'FETCH_PLAYERS':
-      return { loading: false, players: action.payload };
+      return { ...state, loading: false, players: action.payload };
+    case 'DRAFT_PLAYER':
+      const playerId = action.payload;
+      const player = state.players.find(({ id }) => {
+        return id === playerId;
+      });
+      const index = state.players.indexOf(player);
+
+      const selection = state.draftedInfo.selection + 1;
+
+      let team = selection % 10; // 1 % 10 =
+
+      // eslint-disable-next-line no-unused-expressions
+      team === 0 ? (team = 10) : team;
+
+      const round = Math.floor((selection - 1) / 10) + 1;
+
+      if (round % 2 === 0) {
+        team = 11 - team;
+      }
+
+      console.log(`Team: ${team}`);
+      console.log(`Selection: ${selection}`);
+      console.log(`Round: ${round}`);
+
+      state = update(state, {
+        players: {
+          [index]: {
+            drafted: { $set: true },
+            draftedRound: { $set: round },
+            draftedSelection: { $set: selection },
+            draftedTeam: { $set: team }
+          }
+        },
+        draftedInfo: {
+          round: { $set: round },
+          selection: { $set: selection },
+          team: { $set: team }
+        }
+      });
+
+      return { ...state };
     default:
       return state;
   }
